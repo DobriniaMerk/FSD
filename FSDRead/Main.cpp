b1b21187451dc759c9ac76e535e4e1545d943815
Артём Торланов <art.torlan@gmail.com>
2022-10-13 19:02:54 +0300

    added cpp files

---
+++ FSDRead/Main.cpp
@@ -0,0 +1,144 @@
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <iterator>
+#include <windows.h>
+#include <string.h>
+
+sf::Image ReadFile(std::string);
+std::wstring getFile();
+
+int main(int argc, char** argv)
+{
+    sf::String filename;
+
+    for (int i = 0; i < argc; i++)
+    {
+        std::cout << argv[i];
+    }
+
+    if (argc > 1)
+    {
+        std::string f = argv[1];
+        filename = f;
+    }
+    else
+        filename = getFile();
+    
+
+    sf::Image img = ReadFile(filename.toAnsiString());
+
+    sf::RenderWindow window(sf::VideoMode(img.getSize().x, img.getSize().y), "FSD Reader");
+
+    sf::Texture t;
+    t.loadFromImage(img);
+    sf::Sprite s;
+    s.setTexture(t);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(s);
+        window.display();
+    }
+
+    return 0;
+}
+
+sf::Image ReadFile(std::string filename = "out.fsd")
+{
+    sf::Image img;
+    std::ifstream file(filename, std::ios::out | std::ios::binary);
+    unsigned int x, y;
+    unsigned char colornum;
+
+
+    file.read((char*)&x, sizeof(unsigned int));
+    file.read((char*)&y, sizeof(unsigned int));        // get image x and y size
+    file.read((char*)&colornum, sizeof(char));                // number of colors
+    img.create(x, y, sf::Color(255, 0, 0));  // filled with red for debugging
+
+    std::cout << "-------------------" << std::endl;
+    std::cout << "X: " << x << " Y: " << y << std::endl << "Number of colors: " << (int)colornum << std::endl;
+    std::cout << "-------------------" << std::endl;
+
+
+    std::vector<sf::Color> colors;
+
+    for (int i = 0; i < colornum; i++)
+    {
+        unsigned char r, g, b;
+        file.read((char*)&r, sizeof(char));
+        file.read((char*)&g, sizeof(char));
+        file.read((char*)&b, sizeof(char));
+        std::cout << (int)r << ", " << (int)g << ", " << (int)b << std::endl;
+        colors.push_back(sf::Color(r, g, b));
+    }
+
+    std::cout << "-------------------" << std::endl;
+
+    unsigned int n = 0;
+    sf::Color writeColor;
+
+    while (n < y * x)
+    {
+        if (file.eof())
+            break;
+
+        unsigned char num = 0, code = 0;
+        file.read((char*)&num, sizeof(char));
+        file.read((char*)&code, sizeof(char));
+        writeColor = colors[code];
+
+        for (int i = 0; i < num; i++)
+        {
+            img.setPixel(n % x, n / x, writeColor);
+            n++;
+        }
+
+    }
+
+    std::cout << n <<' ' << x * y;
+
+    file.close();
+
+    return img;
+}
+
+std::wstring getFile()
+{
+    // common dialog box structure, setting all fields to 0 is important
+    OPENFILENAME ofn = { 0 };
+    TCHAR szFile[260] = { 0 };
+    // Initialize remaining fields of OPENFILENAME structure
+    ofn.lStructSize = sizeof(ofn);
+    ofn.lpstrFile = szFile;
+    ofn.nMaxFile = sizeof(szFile);
+    ofn.lpstrFilter = L"FSD Image\0 *.FSD\0\0";
+    ofn.nFilterIndex = 1;
+    ofn.lpstrFileTitle = NULL;
+    ofn.nMaxFileTitle = 0;
+    ofn.lpstrInitialDir = NULL;
+    ofn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST;
+
+    std::string s = "";
+    std::wstring empty(s.begin(), s.end());
+
+    if (GetOpenFileName(&ofn) == TRUE)
+    {
+        std::wstring ws(ofn.lpstrFile);
+        std::wcout << ws << std::endl;
+        return ws;
+    }
+    return empty;
+}
---
+++ FSTWrite/Main.cpp
@@ -0,0 +1,118 @@
+﻿#include "Utils.cpp"
+#include <iostream>
+
+#include <windows.h>
+#include <string.h>
+
+std::wstring getFile();
+std::wstring getFolder();
+
+int main()
+{
+    sf::String filename = getFile();
+    
+
+    sf::Image img;
+    img.loadFromFile(filename.toAnsiString());
+
+    int colornum;
+    std::cout << "Input number of colors (must be a power of 2 and no more than 256): ";
+    std::cin >> colornum;
+
+    sf::RenderWindow window(sf::VideoMode(img.getSize().x, img.getSize().y), "Final Image");
+
+
+    std::vector<sf::Color> colors = ImageDithering::Utils::Dither(img, colornum);
+
+    sf::Texture t;
+    t.loadFromImage(img);
+    sf::Sprite s;
+    s.setTexture(t);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+
+            if (event.type == sf::Event::MouseButtonPressed)
+            {
+                sf::String savefolder = getFolder();
+                ImageDithering::Utils::SaveToFile(img, colors, savefolder.toAnsiString());
+                window.close();
+            }
+
+            //  DELETE THIS
+            if (event.type == sf::Event::KeyPressed)
+            {
+                img.saveToFile("out.png");
+                window.close();
+            }
+            //  DELETE THIS
+        }
+
+        window.clear();
+        window.draw(s);
+        window.display();
+    }
+
+    return 0;
+}
+
+std::wstring getFile()
+{
+    // common dialog box structure, setting all fields to 0 is important
+    OPENFILENAME ofn = { 0 };
+    TCHAR szFile[260] = { 0 };
+    // Initialize remaining fields of OPENFILENAME structure
+    ofn.lStructSize = sizeof(ofn);
+    ofn.lpstrFile = szFile;
+    ofn.nMaxFile = sizeof(szFile);
+    ofn.lpstrFilter = L"Image\0*.JPEG;*.JPG;*.PNG;*.BMP;*.TGA;*.PSD;*.HDR;*.PIC\0\0";
+    ofn.nFilterIndex = 1;
+    ofn.lpstrFileTitle = NULL;
+    ofn.nMaxFileTitle = 0;
+    ofn.lpstrInitialDir = NULL;
+    ofn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST;
+
+    std::string s = "";
+    std::wstring empty(s.begin(), s.end());
+
+    if (GetOpenFileName(&ofn) == TRUE)
+    {
+        std::wstring ws(ofn.lpstrFile);
+        std::wcout << ws << std::endl;
+        return ws;
+    }
+    return empty;
+}
+
+std::wstring getFolder()
+{
+    // common dialog box structure, setting all fields to 0 is important
+    OPENFILENAME ofn = { 0 };
+    TCHAR szFile[260] = { 0 };
+    // Initialize remaining fields of OPENFILENAME structure
+    ofn.lStructSize = sizeof(ofn);
+    ofn.lpstrFile = szFile;
+    ofn.nMaxFile = sizeof(szFile);
+    ofn.lpstrFilter = L"FSD Image\0*.fsd\0\0";
+    ofn.nFilterIndex = 1;
+    ofn.lpstrFileTitle = NULL;
+    ofn.nMaxFileTitle = 0;
+    ofn.lpstrInitialDir = NULL;
+    ofn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST;
+
+    std::string s = "";
+    std::wstring empty(s.begin(), s.end());
+
+    if (GetSaveFileName(&ofn) == TRUE)
+    {
+        std::wstring ws(ofn.lpstrFile);
+        std::wcout << ws << std::endl;
+        return ws;
+    }
+    return empty;
+}
